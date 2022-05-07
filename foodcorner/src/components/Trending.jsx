270099b482ee85c0e190f0fec5747dc8dd8cdc20
Fangio <fangiodoom01@gmail.com>
2022-05-07 15:35:18 +0200

    Fetch data

--- foodcorner/src/components/Trending.jsx
+++ foodcorner/src/components/Trending.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+
 function Trending() {
-  return <div>Trending</div>;
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    getTrending();
+  }, []);
+
+  const getTrending = async () => {
+    const check = localStorage.getItem("trending");
+
+    if (check) {
+      setTrending(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("trending", JSON.stringify(data.recipes));
+      setTrending(data.recipes);
+    }
+  };
+
+  return (
+    <div>
+      {trending.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Trending;
